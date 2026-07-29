6c604b4b3bc0e3069c2bd9b4ff01737e4477c2db
Manish Kishore <manishk92611@outlook.com>
2026-07-29 14:49:22 +0530

    Add Color.kt with predefined color values

---
+++ app/src/main/java/com/manishkishore/digidroid/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.manishkishore.digidroid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Slate950 = Color(0xFF020617)
+val Slate900 = Color(0xFF0F172A)
+val Slate800 = Color(0xFF1E293B)
+val Slate100 = Color(0xFFF1F5F9)
+val Slate50 = Color(0xFFF8FAFC)
+val Blue600 = Color(0xFF2563EB)
+val Emerald600 = Color(0xFF059669)
+val Amber600 = Color(0xFFD97706)
+val Rose600 = Color(0xFFE11D48)
